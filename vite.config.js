3d90857aac5c99555796d19b22ee6a9782a4924d
thomasoteixeira <116651793+thomasoteixeira@users.noreply.github.com>
2025-05-02 13:15:06 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/babybet/',
-  plugins: [react()]
+  base: '/babybet/',  // <- este valor é ESSENCIAL
+  plugins: [react()],
 });
